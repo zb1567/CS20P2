40b2d9e66e7f593227aa01c3b62184b7a32e1c97
410015006 <410015006@L815WD220025.edu.cbe.ab.ca>
2026-01-12 10:44:16 -0700

    old stuff 10:44

--- chapter3/src/skillbuilders/Digits.java
+++ chapter3/src/skillbuilders/Digits.java
@@ -8,23 +8,24 @@ public class Digits
 
 	public static void main(String[] args) 
 	{
-		 // Create a Scanner object to read user input
-        Scanner input = new Scanner(System.in);
+		 try (// Create a Scanner object to read user input
+		Scanner input = new Scanner(System.in)) {
+			// Get digits from user
+			    System.out.print("Enter two digit number: ");
+			    int number = input.nextInt();
+			    
+			    int tens, ones;
+			    
+			    tens = number / 10;
+			    ones = number % 10;
+			    
+			    System.out.println("Tens = "+ tens);
+		}
         
         
         
-     // Get digits from user
-        System.out.print("Enter two digit number: ");
-        int number = input.nextInt();
-        
-        int tens, ones;
-        
-        tens = number / 10;
-        ones = number % 10;
-        
-        System.out.println("Tens = "+ tens);
-        
-        System.out.print("Ones = "+ ones);
+     String ones = null;
+	 System.out.print("Ones = "+ ones);
         
 
 	}
